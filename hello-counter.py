1b18880e9212b43feb31c6954a92993d5bd84a82
Elson Rodriguez <elson.rodriguez@gmail.com>
2019-03-14 14:37:33 -0700

    Dat serverless tho

--- hello-counter.py
+++ hello-counter.py
@@ -38,4 +38,5 @@ def hello():
     return response 
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    listen_port = int(os.getenv('PORT', '80'))
+    app.run(host='0.0.0.0', port=listen_port)
